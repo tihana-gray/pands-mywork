8d47471b835da3759f077093d681370c51f340c5
Tihana <tihanagray64@gmail.com>
2025-02-10 22:32:34 +0000

    week02 extra question

---
+++ week02/nameAndAge.py
@@ -0,0 +1,25 @@
+# name and age
+# program that reads a persons name and age and prints hello that persons name and age
+# Author Tihana Gray
+
+
+"""
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"Hello {name}, your age is {age}.")
+"""
+
+"""
+# I have added \t to the name to see how it looks like
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"Hello {name}\t, your age is {age}.")
+"""
+
+"""
+# Program that outputs 21-4
+print(21-4)
+"""
+
+# Program that outputs 2==3
+print(2 == 3)
