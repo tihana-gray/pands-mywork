65b73802dc7148d1204cd37bbfe01de06a10362f
Tihana <tihanagray64@gmail.com>
2025-02-21 17:44:55 +0000

    week03

---
+++ week03/messingwithtypes.py
@@ -0,0 +1,12 @@
+# messing with variable types
+# Author: Tihana Gray
+
+answer = 12
+print (f"answer is {answer}")
+"""
+"""
+#answer_of_question = 11
+
+answer_of_question = 11
+print (f"type of answer is {type(answer_of_question)}")
+print (f"type of type {type(type(answer_of_question))}")
